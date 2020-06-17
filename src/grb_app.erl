3351488e5b1d4af95302ef315fbc2e2e6551c923
ergl <ergl@users.noreply.github.com>
2020-06-17 13:01:06 +0200

    Configure logs, add rotating file and debug logs

--- src/grb_app.erl
+++ src/grb_app.erl
@@ -18,6 +18,7 @@ start(_StartType, _StartArgs) ->
             ok = riak_core:register([{vnode_module, grb_vnode}]),
             ok = riak_core_node_watcher:service_up(grb, self()),
 
+            ok = enable_debug_logs(),
             ok = grb_tcp_server:start_server(),
             case application:get_env(grb, auto_start_background_processes) of
                 {ok, true} ->
@@ -28,6 +29,18 @@ start(_StartType, _StartArgs) ->
             end,
             {ok, Pid}
     end.
+
+-ifdef(debug_log).
+-spec enable_debug_logs() -> ok.
+enable_debug_logs() ->
+    logger:add_handler(debug, logger_std_h, #{
+        filters => [{debug, {fun logger_filters:level/2, {stop, neq, debug}}}],
+        config => #{file => "log/debug.log"}
+    }).
+-else.
+-spec enable_debug_logs() -> ok.
+enable_debug_logs() -> ok.
+-endif.
 
 stop(_State) ->
     ok.
