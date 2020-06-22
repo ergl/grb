d9f9af090006c4d1244a86f52b000517c880f775
ergl <ergl@users.noreply.github.com>
2020-06-22 18:01:06 +0200

    Update todos

--- src/grb_tcp_handler.erl
+++ src/grb_tcp_handler.erl
@@ -18,7 +18,7 @@ process(Promise, 'OpRequest', Args) ->
     #{partition := P, key := K, value := V, snapshot_vc := VC} = Args,
     grb:perform_op(Promise, P, K, VC, V);
 
-%% TODO(borja): Make prepare node parallel
+%% fixme(borja): Make prepare node parallel
 %% See https://medium.com/@jlouis666/testing-a-parallel-map-implementation-2d9eab47094e
 process(Promise, 'PrepareBlueNode', Args) ->
     #{transaction_id := TxId, snapshot_vc := VC, prepares := Prepares} = Args,
@@ -26,7 +26,7 @@ process(Promise, 'PrepareBlueNode', Args) ->
              || #{partition := P, writeset := WS} <- Prepares],
     grb_promise:resolve(Votes, Promise);
 
-%% TODO(borja): Make decide node parallel
+%% fixme(borja): Make decide node parallel
 %% See https://medium.com/@jlouis666/testing-a-parallel-map-implementation-2d9eab47094e
 process(_Promise, 'DecideBlueNode', Args) ->
     #{transaction_id := TxId, partitions := Ps, commit_vc := CVC} = Args,
