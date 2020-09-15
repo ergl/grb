6b3eacc8bc24f77ced86fa75c571c652c7123d75
ergl <ergl@users.noreply.github.com>
2020-09-15 13:02:38 +0200

    Update red defaults

--- test/grb_utils.erl
+++ test/grb_utils.erl
@@ -135,10 +135,10 @@ start_node(Name, Config) ->
             ok = erpc:call(Node, application, set_env, [grb, local_broadcast_interval, 5]),
             ok = erpc:call(Node, application, set_env, [grb, op_prepare_wait_ms, 5]),
             ok = erpc:call(Node, application, set_env, [grb, prune_committed_blue_interval, 50]),
-            ok = erpc:call(Node, application, set_env, [grb, red_heartbeat_interval, 50]),
-            ok = erpc:call(Node, application, set_env, [grb, red_delivery_interval, 100]),
-            ok = erpc:call(Node, application, set_env, [grb, red_prune_interval, 500]),
-            %% todo(borja, red): remove
+
+            ok = erpc:call(Node, application, set_env, [grb, red_heartbeat_interval, 5]),
+            ok = erpc:call(Node, application, set_env, [grb, red_delivery_interval, 10]),
+            ok = erpc:call(Node, application, set_env, [grb, red_prune_interval, 20]),
             ok = erpc:call(Node, application, set_env, [grb, red_coord_pool_size, 500]),
 
             {ok, _} = erpc:call(Node, grb, start, []),
